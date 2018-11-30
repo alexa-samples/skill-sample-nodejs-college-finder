279786bd8b0ffcf27f15dd5a23772bdb3d818ce5
Stef Sharp <sharstef@amazon.com>
2018-11-30 15:14:31 -0800

    Initializing schools as an empty array for undefined

--- lambda/custom/resultsHandlers.js
+++ lambda/custom/resultsHandlers.js
@@ -55,13 +55,13 @@ function pageResults (handlerInput, attributes) {
   // Determine if this is the favorites or result list
   if (attributes[constants.STATE] === constants.STATES.FAVORITES) {
     page = attributes[constants.LIST_PAGE];
-    schools = attributes[constants.LIST];
+    schools = attributes[constants.LIST] ? attributes[constants.LIST] : [];
     end = 'FAVORITES_NO_MORE_ITEMS';
     prompt = 'FAVORITES_CURRENT_ITEM_PROMPT';
     title = 'FAVORITES_TITLE';
   } else {
     page = attributes[constants.SEARCH_PAGE];
-    schools = attributes[constants.SEARCH_RESULTS];
+    schools = attributes[constants.SEARCH_RESULTS] ? attributes[constants.SEARCH_RESULTS] : [];
     end = 'LIST_SCHOOLS_END_OF_LIST';
     prompt = 'LIST_SCHOOLS_PROMPT';
     title = 'RESULTS_TITLE';
@@ -427,7 +427,11 @@ module.exports = {
           .getResponse();
       }
 
-      if (attributes[constants.CURRENT_SCHOOL_ID] === attributes[constants.CURRENT_SCHOOL].id) {
+      if (
+        attributes[constants.CURRENT_SCHOOL] &&
+        attributes[constants.CURRENT_SCHOOL_ID] &&
+        attributes[constants.CURRENT_SCHOOL_ID] === attributes[constants.CURRENT_SCHOOL].id
+      ) {
         school = attributes[constants.CURRENT_SCHOOL];
       } else {
         const item = list.find(listItem => {
@@ -454,10 +458,7 @@ module.exports = {
         helpers.saveUser(handlerInput, attributes, 'session');
         helpers.saveUser(handlerInput, attributes, 'persistent');
 
-        return handlerInput.responseBuilder
-          .speak(message)
-          .reprompt(message)
-          .getResponse();
+        return handlerInput.responseBuilder.speak(message).reprompt(message).getResponse();
       } else {
         return notFound(handlerInput, attributes);
       }
@@ -569,10 +570,7 @@ module.exports = {
         helpers.saveUser(handlerInput, attributes, 'session');
         helpers.saveUser(handlerInput, attributes, 'persistent');
 
-        return handlerInput.responseBuilder
-          .speak(message)
-          .reprompt(message)
-          .getResponse();
+        return handlerInput.responseBuilder.speak(message).reprompt(message).getResponse();
       }
     }
   },
